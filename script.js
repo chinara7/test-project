1411b003daef1f046e3e637a385a72bdf8a7e700
Chinara_Huseyn <80186374+chinara7@users.noreply.github.com>
2022-05-10 10:35:31 +0400

    style.css elave edildi

---
+++ script.js
@@ -0,0 +1,7 @@
+// TERANARY OPERATOR '?'
+
+// condition? expression1: expression2;
+
+// let marks = prompt("Balinizi daxil edin:");
+// let result;
+// result = marks > 50 ? "kecdiniz" : "kesildiniz";
